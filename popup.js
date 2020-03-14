ca1c0daf59fffd15129b72f5d5209b0a5ac99a84
domsupamoe <0jinwei0.wang@gmail.com>
2020-03-13 20:38:30 -0700

    Add active and inactive icons to button toggle event

--- popup.js
+++ popup.js
@@ -20,8 +20,24 @@ toggleBtn.addEventListener('click', () => {
     let enabled = !data.enabled;
     if (enabled) {
       toggleBtn.style.backgroundColor = btnColors.active;
+      chrome.browserAction.setIcon({
+        path: {
+          '16': "images/tab_active16.png",
+          '32': "images/tab_active32.png",
+          '48': "images/tab_active48.png",
+          '128': "images/tab_active128.png"
+        }
+      });
     } else {
       toggleBtn.style.backgroundColor = btnColors.inactive;
+      chrome.browserAction.setIcon({
+        path: {
+          '16': "images/tab_inactive16.png",
+          '32': "images/tab_inactive32.png",
+          '48': "images/tab_inactive48.png",
+          '128': "images/tab_inactive128.png",
+        }
+      });
     }
     chrome.storage.local.set({enabled: enabled});
   });
