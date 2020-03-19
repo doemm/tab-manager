2320d41d388c41d505b9e469747f33c2e7e06ada
domsupamoe <0jinwei0.wang@gmail.com>
2020-03-19 02:34:18 -0700

    Refine the code to follow certain style and standard

--- popup.js
+++ popup.js
@@ -22,20 +22,20 @@ toggleBtn.addEventListener('click', () => {
       toggleBtn.style.backgroundColor = btnColors.active;
       chrome.browserAction.setIcon({
         path: {
-          '16': "images/tab_active16.png",
-          '32': "images/tab_active32.png",
-          '48': "images/tab_active48.png",
-          '128': "images/tab_active128.png"
+          '16': 'images/tab_active16.png',
+          '32': 'images/tab_active32.png',
+          '48': 'images/tab_active48.png',
+          '128': 'images/tab_active128.png'
         }
       });
     } else {
       toggleBtn.style.backgroundColor = btnColors.inactive;
       chrome.browserAction.setIcon({
         path: {
-          '16': "images/tab_inactive16.png",
-          '32': "images/tab_inactive32.png",
-          '48': "images/tab_inactive48.png",
-          '128': "images/tab_inactive128.png",
+          '16': 'images/tab_inactive16.png',
+          '32': 'images/tab_inactive32.png',
+          '48': 'images/tab_inactive48.png',
+          '128': 'images/tab_inactive128.png',
         }
       });
     }
@@ -53,7 +53,7 @@ sortWindow.addEventListener('click', () => {
       const itemsLen = urlItems.length;
 
       const info = {
-        domain: "",
+        domain: '',
         id: tab.id
       };
 
