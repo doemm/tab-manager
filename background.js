4ad3cd963185b20a3e45d538b040bf2b85986fc8
domsupamoe <0jinwei0.wang@gmail.com>
2020-03-28 15:52:52 -0700

    Add a new feature for displaying tabs in current window

--- background.js
+++ background.js
@@ -2,6 +2,20 @@
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.set({enabled: true});
+  chrome.storage.local.set({savedTab: 'actions-tab'});
+  chrome.storage.local.set({prevWindowId: -1});
+});
+
+chrome.windows.onFocusChanged.addListener((windowId) => {
+  chrome.storage.local.get('prevWindowId', (data) => {
+    let prevWindowId = data.prevWindowId;
+    if (prevWindowId === -1) prevWindowId = windowId;
+    if (windowId !== -1 && windowId != prevWindowId) {
+      prevWindowId = windowId;
+      chrome.storage.local.set({savedTab: 'actions-tab'});
+    }
+    chrome.storage.local.set({prevWindowId: prevWindowId});
+  });
 });
 
 chrome.tabs.onUpdated.addListener((currTabId, currChanges, currTab) => {
--- popup.js
+++ popup.js
@@ -3,24 +3,38 @@
 const colorMap = {
   blue: '#0377fb',
   grey: '#929292',
-  red: '#f44336'
+  red:  '#f44336'
 };
+
+chrome.storage.local.get('savedTab', (data) => {
+  let savedTab = data.savedTab;
+  if (savedTab !== 'actions-tab') {
+    let tabButton = document.querySelector('#'+savedTab);
+    tabButton.click();
+  }
+});
 
 let tabBar = document.querySelector('.tab-bar');
 tabBar.addEventListener('click', (event) => {
+  let activeTab = event.target;
+
   let tabContents = document.querySelectorAll('.tab-content');
   for (let i = 0; i < tabContents.length; ++i) {
-    tabContents[i].style.display = 'none';
+    if (tabContents[i].id !== activeTab.dataset.page) {
+      tabContents[i].style.display = 'none';
+    }
   }
   let tabButtons = document.querySelectorAll('.tab-button');
   for (let i = 0; i < tabButtons.length; ++i) {
-    tabButtons[i].style.background = colorMap.grey;
+    if (tabButtons[i].id !== activeTab.id) {
+      tabButtons[i].style.background = colorMap.grey;
+    }
   }
 
-  let activeTab = event.target;
   activeTab.style.background = colorMap.red;
   switch(activeTab.id) {
     case 'currwin-tab':
+      loadCurrwinPage();
       let currwinPage = document.querySelector('#currwin-page');
       currwinPage.style.display = 'block';
       break;
@@ -32,6 +46,7 @@ tabBar.addEventListener('click', (event) => {
       let actionsPage = document.querySelector('#actions-page');
       actionsPage.style.display = 'block';
   }
+  chrome.storage.local.set({savedTab: activeTab.id});
 });
 
 let toggleBtn = document.querySelector('#toggle-button');
@@ -126,4 +141,29 @@ sortBtn.addEventListener('click', () => {
       chrome.tabs.move(domains[i].id, {index: i});      
     }
   });
+});
+
+function loadCurrwinPage() {
+  let currwinPage = document.querySelector('#currwin-page');
+  currwinPage.innerHTML = '';
+  chrome.tabs.query({currentWindow: true}, (tabs) => {
+    let ulElement = document.createElement('ul');
+    ulElement.className = 'site-list';
+    for (let tab of tabs) {
+      let liElement = document.createElement('li');
+      liElement.className = 'site-item';
+      liElement.dataset.tabId = tab.id;
+      liElement.innerHTML = '<img class="site-icon" ' +
+        'src="' + tab.favIconUrl + '">' + tab.title;
+      ulElement.appendChild(liElement);
+    }
+    currwinPage.appendChild(ulElement);
+  });
+}
+
+let currwinPage = document.querySelector('#currwin-page');
+currwinPage.addEventListener('click', (event) => {
+  const siteItem = event.target;
+  const tabId = siteItem.dataset.tabId;
+  chrome.tabs.update(parseInt(tabId), {active: true});
 });
